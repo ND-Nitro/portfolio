0009555f440269bf94143feba0f9bd0f72405c9c
ND-Nitro <steffenheltne@hotmail.com>
2026-02-26 22:58:21 +0100

    changed out div with nav in html and added new class for the buttons

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 const routes = {
-  "/index.html": "<h2>Home</h2>",
-  "/projects": "<h2>Mine Prosjekter</h2>",
-  "/contact": "<h2>Kontakt Meg</h2>",
+  "/": "<h2>Home</h2>",
+  "/projects": "<h2>Projects</h2>",
+  "/contact": "<h2>contact</h2>",
 };
 
 const app = document.getElementById("app");
 
-function navigateTo(url) {
-  history.pushState(null, null, url);
-  router();
+function getPath() {
+  const hash = window.location.hash || "#/";
+  return hash.replace("#", "") || "/";
 }
 
 function router() {
-  const path = window.location.hash.slice(1) || "/";
-  app.innerHTML = routes[path] || "<h2>404</h2>";
+  const path = getPath();
+  app.innerHTML = routes[path] || "<h2>404 Not Found</h2>";
 }
-
-document.addEventListener("click", (e) => {
-  if (e.target.matches("[data-link]")) {
-    window.Location.hash = e.target.getAttribute("data-link");
-  }
-});
 
 window.addEventListener("hashchange", router);
 router();

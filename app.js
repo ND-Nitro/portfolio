f6614c213d1e4f8f61b9c73dd826caa081f889ea
ND-Nitro <steffenheltne@hotmail.com>
2026-02-28 11:15:51 +0100

    added new sections + css +js

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
 const routes = {
-  "/": "<h2>Home</h2>",
-  "/projects": "<h2>Projects</h2>",
-  "/contact": "<h2>contact</h2>",
+  "/": `
+    <section class="section section--big">
+      <h2>Home</h2>
+      <p>Home content goes here.</p>
+    </section>
+
+    <section class="section">
+      <h2>About</h2>
+      <p>Some info about me.</p>
+    </section>
+
+    <section class="section">
+      <h2>Skills</h2>
+      <p>HTML • CSS • JS • FIGMA</p>
+    </section>
+  `,
+
+  "/projects": `
+    <section class="section">
+      <h2>Projects</h2>
+      <p>Project content here.</p>
+    </section>
+  `,
+
+  "/contact": `
+    <section class="section">
+      <h2>Contact</h2>
+      <p>Contact info here.</p>
+    </section>
+  `,
 };
 
 const app = document.getElementById("app");
@@ -13,8 +40,22 @@ function getPath() {
 
 function router() {
   const path = getPath();
-  app.innerHTML = routes[path] || "<h2>404 Not Found</h2>";
+
+  app.innerHTML =
+    routes[path] ||
+    `
+      <section class="section">
+        <h2>404 - Not Found</h2>
+      </section>
+    `;
+
+  app.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
 }
 
 window.addEventListener("hashchange", router);
+
+// first load
 router();
